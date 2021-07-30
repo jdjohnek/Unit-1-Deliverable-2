d5029c7b4330f4bad71e55d8b75ebf449e1cffd4
Jarod <jdjohnek@gmail.com>
2021-07-29 21:39:51 -0400

    Second commit for deliverable two

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-//1. Prompts the user for the pet's name
+//Prompts the user for the pet's name
 let petName = prompt("Name your pet");
-
+let energy = 0;
+let happiness = 0;
 //Prompts the user six times for an action.
 for (i=0; i<6; i++){
     var choice = prompt ("feed, pet, or walk?");
-}   
+  
 //"feed" increases energy by 2.
-// loop here?
-if (choice === "feed") {
+if (choice === "feed")
     (energy += 2);
-}
 
 //"pet"increases happiness by 1.
-// loop here?
-if (choice === "pet") {
+if (choice === "pet")
     (happiness += 1);
-}
-//"walk" increases happiness by 2 and decreases energy by 1.
-// loop here?
-if (choice === "walk") {
-    (happiness += 2); 
-    (energy -= 1);
-}  
-//Alerts the user if there is not enough energy for a walk.
-if (energy <= 0) {
-    alert("Not enough energy to walk")
-} else if (energy > 0) 
-    alert = ('null');
 
+//"walk" increases happiness by 2 and decreases energy by 1.
+if (choice === "walk")
+    (happiness += 2);
+
+if (choice === "walk")
+     (energy -= 1);
+
+//Alerts the user if there is not enough energy for a walk.
+if (energy < 0) 
+    alert("Not enough energy to walk");
+ else if (energy >= 1)
+    alert = ('null');
+}
 
 
 //Does not affect happiness or energy if there is not enough energy for a walk.
@@ -39,5 +38,3 @@ if (energy <= 0) {
 correct sentence format: _______ has __ happiness and __ energy.
 */
 console.log(petName + " has " + (happiness) + " happiness and " + (energy) + " energy")
-
-// This is the best I could do with the material I had. Finding the correct solution on the suggested websites & Google is like trying to find a needle in a haystack.
